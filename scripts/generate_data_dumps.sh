cac7ef942d0d02f94ccb448925f450166ac10d84
eeue56 <enalicho@gmail.com>
2025-02-21 22:26:01 +0100

    filter data down to better results

--- scripts/generate_data_dumps.sh
+++ scripts/generate_data_dumps.sh
@@ -2,27 +2,29 @@
 
 mkdir -p src/data
 
+claim_review_json_file="./raw_data/claim_reviews.json"
+
 # Norway
 
-cat claim_reviews.json | jq '[.[] | select( .fact_checker.name == "faktiskno" and (.label == "credible"))] | .[:50]' > src/data/norwegian_credible.json
+cat "${claim_review_json_file}" | jq '[.[] | select( .fact_checker.name == "faktiskno" and (.label == "credible"))] | .[:50]' > src/data/norwegian_credible.json
 
-cat claim_reviews.json | jq '[.[] | select( .fact_checker.name == "faktiskno" and (.label == "not_credible"))] | .[:50]' > src/data/norwegian_not_credible.json
+cat "${claim_review_json_file}" | jq '[.[] | select( .fact_checker.name == "faktiskno" and (.label == "not_credible"))] | .[:50]' > src/data/norwegian_not_credible.json
 
 # USA
 
-cat claim_reviews.json | jq '[.[] | select((.claim_text[0] | contains("Says") | not) and (.claim_text[0] | contains("On") | not) and .fact_checker.name == "politifact" and (.label == "credible")) ] | .[:50]'  > src/data/usa_credible.json
+cat "${claim_review_json_file}" | jq '[.[] | select((.claim_text[0] | contains("Says") | not) and (.claim_text[0] | contains("On") | not) and (.claim_text[0] | contains("http://") | not) and (.claim_text[0] | contains("https://") | not) and .fact_checker.name == "politifact" and (.label == "credible")) ] | .[:50]'  > src/data/usa_credible.json
 
-cat claim_reviews.json | jq '[.[] | select((.claim_text[0] | contains("Says") | not) and (.claim_text[0] | contains("On") | not) and .fact_checker.name == "politifact" and (.label == "not_credible")) ] | .[:50]'  > src/data/usa_not_credible.json
+cat "${claim_review_json_file}" | jq '[.[] | select((.claim_text[0] | contains("Says") | not) and (.claim_text[0] | contains("On") | not) and (.claim_text[0] | contains("http://") | not) and (.claim_text[0] | contains("https://") | not) and .fact_checker.name == "politifact" and (.label == "not_credible")) ] | .[:50]'  > src/data/usa_not_credible.json
 
 # UK
 
-cat claim_reviews.json | jq '[.[] | select(.fact_checker.name == "full-fact" and (.label == "credible"))] | .[:50]' > src/data/uk_credible.json
+cat "${claim_review_json_file}" | jq '[.[] | select(.fact_checker.name == "full-fact" and (.label == "credible"))] | .[:50]' > src/data/uk_credible.json
 
-cat claim_reviews.json | jq '[.[] | select(.fact_checker.name == "full-fact" and (.label == "not_credible"))] | .[:50]' > src/data/uk_not_credible.json
+cat "${claim_review_json_file}" | jq '[.[] | select(.fact_checker.name == "full-fact" and (.label == "not_credible"))] | .[:50]' > src/data/uk_not_credible.json
 
 # Sweden
 
-cat claim_reviews.json |
+cat "${claim_review_json_file}" |
     jq '[.[]
         | select(
             (
@@ -54,7 +56,7 @@ cat claim_reviews.json |
         ]
     | .[:50]' > src/data/sweden_credible.json
 
-cat claim_reviews.json |
+cat "${claim_review_json_file}" |
     jq '[.[]
         | select(
             (
